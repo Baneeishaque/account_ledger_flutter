edc4fd27a1ed74448791bada7300fa62c029821d
Banee Ishaque K <baneeishaque@gmail.com>
2022-07-12 04:58:23 +0530

    Text-Field & Scaffold Fix

--- lib/LoginForm2.dart
+++ lib/LoginForm2.dart
@@ -9,65 +9,61 @@ class LoginForm2 extends StatefulWidget {
 }
 
 class LoginForm2State extends State<LoginForm2> {
-  final _formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _formKey,
-        child: Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  BeautyTextfield(
-                    width: double.maxFinite,
-                    height: 60,
-                    duration: Duration(milliseconds: 300),
-                    inputType: TextInputType.text,
-                    prefixIcon: Icon(
-                      Icons.lock_outline,
-                    ),
-                    placeholder: "With Suffix Icon",
-                    onTap: () {
-                      print('Click');
-                    },
-                    onChanged: (t) {
-                      print(t);
-                    },
-                    onSubmitted: (d) {
-                      print(d.length);
-                    },
-                    suffixIcon: Icon(Icons.remove_red_eye),
+    return Scaffold(body: Builder(builder: (BuildContext buildContext) {
+      return Padding(
+          padding: EdgeInsets.only(left: 16.0, right: 16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                BeautyTextfield(
+                  width: double.maxFinite,
+                  height: 60,
+                  duration: Duration(milliseconds: 300),
+                  inputType: TextInputType.text,
+                  prefixIcon: Icon(
+                    Icons.lock_outline,
                   ),
-                  BeautyTextfield(
-                    width: double.maxFinite,
-                    height: 60,
-                    duration: Duration(milliseconds: 300),
-                    inputType: TextInputType.text,
-                    prefixIcon: Icon(
-                      Icons.lock_outline,
-                    ),
-                    placeholder: "Without Suffix Icon",
-                    onTap: () {
-                      print('Click');
-                    },
-                    onChanged: (t) {
-                      print(t);
-                    },
-                    onSubmitted: (d) {
-                      print(d.length);
-                    },
+                  placeholder: "With Suffix Icon",
+                  onTap: () {
+                    print('Click');
+                  },
+                  onChanged: (t) {
+                    print(t);
+                  },
+                  onSubmitted: (d) {
+                    print(d.length);
+                  },
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+                BeautyTextfield(
+                  width: double.maxFinite,
+                  height: 60,
+                  duration: Duration(milliseconds: 300),
+                  inputType: TextInputType.text,
+                  prefixIcon: Icon(
+                    Icons.lock_outline,
                   ),
-                  RaisedButton(
-                    onPressed: () {
-                      if (_formKey.currentState.validate()) {
-                        Scaffold.of(context).showSnackBar(
-                            SnackBar(content: Text('Processing Data...')));
-                      }
-                    },
-                    child: Text('Submit...'),
-                  )
-                ])));
+                  placeholder: "Without Suffix Icon",
+                  onTap: () {
+                    print('Click');
+                  },
+                  onChanged: (t) {
+                    print(t);
+                  },
+                  onSubmitted: (d) {
+                    print(d.length);
+                  },
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Scaffold.of(buildContext).showSnackBar(
+                        SnackBar(content: Text('Processing Data...')));
+                  },
+                  child: Text('Submit...'),
+                )
+              ]));
+    }));
   }
 }
